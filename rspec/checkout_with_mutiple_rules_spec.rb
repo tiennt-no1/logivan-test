de52637e4dadfcb0a879f1bdafb80e312d5210a5
tien-no1 <tiennt.june.20@gmail.com>
2019-08-15 11:54:49 +0700

    test case contain apply item and not

--- rspec/checkout_with_mutiple_rules_spec.rb
+++ rspec/checkout_with_mutiple_rules_spec.rb
@@ -29,6 +29,8 @@ RSpec.describe LogivanTest::Checkout do
   let (:item_price) { 70.0 }
   let (:discount_point) { 70.0 }
   let (:apply_code) { '001' }
+  let (:apply_code2) { '002' }
+  let (:unapply_code) { '003' }
   let (:percent_discount) { 10 }
 
   def create_sample_item
@@ -113,4 +115,26 @@ RSpec.describe LogivanTest::Checkout do
     ]
     expect{LogivanTest::Checkout.new(promotion_rules)}.to raise_error('Error: Cannot applied mutiple rules for 1 item code')
   end
+
+  it 'Apply drop price for 2 items' do
+    promotion_rules = [
+      LogivanTest::DiscountByAmountSpecificItemRule.new(
+        apply_code: apply_code, amount_items: 1, discount_price: discount_price
+      ),
+      LogivanTest::DiscountByAmountSpecificItemRule.new(
+        apply_code: apply_code2, amount_items: 1, discount_price: discount_price
+      )
+    ]
+    checkout = LogivanTest::Checkout.new(promotion_rules)
+    item1 =  create_sample_item
+    checkout.scan item1
+    item2 = create_sample_item; item2.code = apply_code2
+    checkout.scan item2
+    expect(checkout.total).to equal(discount_price*2) # because both 2 items is discount
+
+    item3 = create_sample_item; item2.code = unapply_code
+    checkout.scan item3
+    expect(checkout.total).to equal(discount_price*2+item_price) # 2 items discount, 1 not
+  end
 end
+
